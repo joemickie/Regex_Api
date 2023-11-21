35c60a19fb018170c849fb0f75a2144f37fa62e4
wikiwoo <wikiwoo@wikiwoos-MacBook-Pro.local>
2023-11-21 16:03:32 +0100

    Updated lombok

---
+++ src/main/java/com/example/regex_api/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.example.regex_api.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
---
+++ src/test/java/com/example/regex_api/RegexApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.regex_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegexApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
